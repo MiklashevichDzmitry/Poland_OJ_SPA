1643e405cb8b4d23ab29bb1113fb23f163f280ca
MiklashevichDzmitry <anfisayou1997@gmail.com>
2017-08-30 00:01:28 +0300

    undo

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ const env = process.env.NODE_ENV || 'development';
 const httpPort = 80;
 const httpsPort = 443;
 const devPort = 3000;
-let port = devPort;
+let port = httpsPort ;
 let resumeCounter = 0;
 
 if (env === 'production') {
-  port = devPort; // or httpsPort
+  port = httpsPort; // or httpsPort
 }
 
 app.use(express.static('public'));
